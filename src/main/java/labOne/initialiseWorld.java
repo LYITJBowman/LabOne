d3f61b307b3f805e97430d0b1f29db1eb263ed4a
JBowman <L00170244@student.lyit.ie>
2021-10-18 21:41:32 +0100

    Updated test code

---
+++ src/main/java/labOne/initialiseWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package labOne;
+
+/**
+ * @author james
+ *
+ */
+public class initialiseWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Welcome adventurer");
+
+	}
+
+}
---
+++ src/test/java/labOne/initialiseWorld_Test.java
@@ -0,0 +1,34 @@
+/** 
+ * 
+ */
+package labOne;
+
+import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Test;
+
+import mvnSimpleProj.App;
+
+/**
+ * @author james
+ *
+ */
+public class initialiseWorld_Test {
+
+	/**
+	 * Test method for {@link labOne.initialiseWorld#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		
+		initialiseWorld.main(null);
+		
+		assertEquals("Welcome adventurer" + System.getProperty("line.separator"), outContent.toString());
+		
+		 
+	}
+
+}
